0c634ab3dd3c6a5c51aea3f9d949335aaabe855c
Seba <explorer223@outlook.com>
2023-05-20 19:46:53 +0200

    Initial commit

---
+++ ExampleList.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ShoppingList
+{
+    public class ExampleList
+    {
+        //pole  Person  person1 
+        private  string NameOfThing;
+        private  int amount;
+        private  decimal price;
+
+        public ExampleList() 
+        {
+            NameOfThing = "NOT DEFINE";
+            amount = 0;
+            price = 0;
+        }
+    }
+}
---
+++ Person.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ShoppingList
+{
+    public class Person
+    {
+        public string FirstName; // zwykle pola od 11 do 13 
+        public string Surname;
+        public Person() // konstruktor 
+        {
+            FirstName = "Not Define";
+            Surname = "Not Define";
+        }
+    }
+}
