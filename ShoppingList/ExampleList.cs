42172b9d32558a502648080f22fcf66cec6165db
Seba <explorer223@outlook.com>
2023-05-24 22:51:24 +0200

    Add new Method and Inherit
    Added new Method in Article Clas plus added some fields , first tries
    with Inherit

--- ShoppingList/ExampleList.cs
+++ ShoppingList/ExampleList.cs
@@ -7,13 +7,13 @@ using System.Text;
 using System.Threading.Tasks;
 namespace ShoppingList
 {
-    public class ExampleList
+    public class ExampleList : Articles
     {
         public string? FirstName { get; init; }
         public string? LastName { get; init; }
         public string? NameOfList { get; init; }
         public DateTime? LastDate { get; init; }
-
+        //public List<Articles>? article = new();
         public ExampleList(string firstName, string lastName , string nameOfList , DateTime created) 
         {
             FirstName = firstName;
